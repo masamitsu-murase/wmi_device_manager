7b1342376759c5c6dffd866c16b38048eaa4c7a8
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-09-08 02:31:39 +0900

    Format source code files.

--- wmidevicemanager/__init__.py
+++ wmidevicemanager/__init__.py
@@ -1,5 +1,5 @@
-
-from .wmidevicemanager import WmiDeviceManager, yellow_bang_devices, find_device
+from .wmidevicemanager import (WmiDeviceManager, yellow_bang_devices,
+                               find_device)
 from .win32pnpentity import Win32PnpEntity
 
 __version__ = "1.2.0"
