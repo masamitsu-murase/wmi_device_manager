5197e6f33384874d61c3ec03c0cd27e5f24c15b7
Masamitsu MURASE <masamitsu.murase@gmail.com>
2019-09-14 21:38:56 +0900

    Support __all__.

--- wmidevicemanager/__init__.py
+++ wmidevicemanager/__init__.py
@@ -2,4 +2,8 @@ from .wmidevicemanager import (WmiDeviceManager, error_devices, find_device,
                                yellow_bang_devices)
 from .win32pnpentity import Win32PnpEntity
 
+__all__ = [
+    "WmiDeviceManager", "error_devices", "find_device", "yellow_bang_devices",
+    "Win32PnpEntity"
+]
 __version__ = "1.2.1"
